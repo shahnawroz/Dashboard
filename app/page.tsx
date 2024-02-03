8749d2c72fd1a10d3c1021f59f165db0e003898d
shahnawroz <sh.nawrose@gmail.com>
2024-02-03 22:07:24 +0600

    test

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Dashboard.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-             Welcome 
+              Next.js Learn Course
             </a>
             , brought to you by Nawroz.
           </p>
